b041003382c38efc05ee38b71c24093a61c11728
Hans-Peter Doerr <doerr@kis.uni-freiburg.de>
2013-02-04 19:47:59 +0100

    kernel: add earth_fixed, earth_itrf text kernels

---
+++ src/earth_fixed.tf
@@ -0,0 +1,10 @@
+ 
+         \begindata
+
+           TKFRAME_EARTH_FIXED_RELATIVE = 'IAU_EARTH'
+           TKFRAME_EARTH_FIXED_SPEC     = 'MATRIX'
+           TKFRAME_EARTH_FIXED_MATRIX   = ( 1   0   0
+                                            0   1   0
+                                            0   0   1 )
+
+         \begintext
---
+++ src/earth_itrf.tf
@@ -0,0 +1,19 @@
+	Recall that the frame `EARTH_FIXED' is a TK frame.  As a result
+        its relationship to other frames must be specified via
+        a kernel pool variable.  We make that specification here.
+
+ 
+         Use the definition below if you have a high precision ITRF93 binary
+         pck file.
+   
+
+         \begindata
+	   OBJECT_EARTH_FRAME =  'ITRF93'
+           TKFRAME_EARTH_FIXED_RELATIVE = 'ITRF93'
+           TKFRAME_EARTH_FIXED_SPEC     = 'MATRIX'
+           TKFRAME_EARTH_FIXED_MATRIX   = ( 1   0   0
+                                            0   1   0
+                                            0   0   1 )
+
+
+         \begintext
