14b05a6e50c1962542c4e35e5823858de250fe69
Hans-Peter Doerr <doerr@kis.uni-freiburg.de>
2013-02-05 21:51:13 +0100

    added older earth EOP kernel that can be combined with a current one

--- src/getkernels.sh
+++ src/getkernels.sh
@@ -10,7 +10,8 @@ kernels="http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0010.tls
   http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc 
   http://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de421.bsp
   http://naif.jpl.nasa.gov/pub/naif/generic_kernels/fk/planets/earth_assoc_itrf93.tf
-  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc"
+  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc
+  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/a_old_versions/earth_720101_070527.bpc"
 
 
 mkdir _tmp_ || die "could not create tempdir"
