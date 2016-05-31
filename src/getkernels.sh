fa89d3a4b7f3fc8d2336168647bf45ea0d099d66
Hans-Peter Doerr <doerr@mps.mpg.de>
2016-05-31 17:02:57 +0200

    updated to latest online-kernels

--- src/getkernels.sh
+++ src/getkernels.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
+owd=$PWD
 
 die ()
 {
     echo "$1"
+    cd $owd
     exit 1
 }
 
-kernels="http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0010.tls 
+kernels="http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0011.tls 
   http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/pck00010.tpc 
-  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de421.bsp
+  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430.bsp
   http://naif.jpl.nasa.gov/pub/naif/generic_kernels/fk/planets/earth_assoc_itrf93.tf
   http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_latest_high_prec.bpc
-  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/a_old_versions/earth_720101_070527.bpc"
+  http://naif.jpl.nasa.gov/pub/naif/generic_kernels/pck/earth_070425_370426_predict.bpc"
 
+kern_tmp=kernels_tmp
 
-( cd tmp_ && wget $kernels ) || die "could not spice kernels"
+test -d $kern_tmp || mkdir -p $kern_tmp
 
-
-
-
+cd $kern_tmp || die "could not enter temp kernel dir"
+rm -f *.tls *.tpc *.bsp *.tf *.bpc
+wget $kernels || die "ERROR while getting the kernels"
+cd $owd
